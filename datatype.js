a49ecf41a83c5e99c9d1cab241efc5e1408560e6
AMRISH SHARMA <127399235+amrish1255@users.noreply.github.com>
2024-02-25 03:22:45 +0000

    datatype

---
+++ datatype.js
@@ -0,0 +1,21 @@
+// Data type are two in javaScript
+
+/* 1=> Premetive data type
+
+   2=> Non Premetive datat type */
+
+/* premetive data type*/
+
+// Number => range b/t  to power 53
+// String =>" "
+// Boolean => true/false
+//null => stand alone
+// undefined => undefined
+// symbol => unique ( use for uniquiness find );
+
+/* Non_Premitive Data type */
+// object
+
+console.log(typeof "Hitesh");
+console.log(typeof null);
+console.log(typeof undefined);
