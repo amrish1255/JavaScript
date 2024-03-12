21c3e3d89003373a3b0f14209a4cfc6cadc206d2
AMRISH SHARMA <127399235+amrish1255@users.noreply.github.com>
2024-03-12 17:21:47 +0530

    Variable
    variable

---
+++ variable.js
@@ -0,0 +1,14 @@
+// // variable
+// 1 let
+// 2 cons
+// 3 var(but this is not use in ES6)
+
+// use of var variable
+var myName = "hello";
+myName = "jaan";
+var $1_my1_$$Age = 12;
+var _1my__NextName = "Raghav";
+
+console.log(myName);
+console.log($1_my1_$$Age);
+console.log(_1my__NextName);
